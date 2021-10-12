96cf4af9deee2709203dffebcaa7105259b628f2
Spoonska <spencerkyle@Spencers-MacBook-Pro.local>
2021-10-11 23:17:21 -0600

    finished branch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -22,15 +22,28 @@ var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     // make a request to the url
-    fetch (apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-        });
-    });
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) {
+        //notice this
+        alert("Unable to connect to GitHub");
+    })
 };
 
 // display repos
 var displayRepos = function(repos, searchTerm) {
+    //check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
     //clear out old content
     repoContainerEl.textContent = "";
     repoSearchTerm.textContent = searchTerm;
